9855a128151130e25494d7dd298bf35dc4be36c8
khader <khdrshk4@gmail.com>
2016-08-14 17:31:53 +0530

    Create feed.php

---
+++ feed.php
@@ -0,0 +1,19 @@
+----Basic structure of RSS feed----
+
+<?xml version='1.0' encoding='UTF-8'?>
+<rss version='2.0'>
+<channel>
+
+<title>Website Title</title>
+<link>Website URL</link>
+<description>Website description</description>
+<language>en-us</language>
+
+<item>
+<title>Item/Article Title</title>
+<link>Item/Article URL</link>
+<description>Items/Article description</description>
+</item>
+
+</channel>
+</rss>
